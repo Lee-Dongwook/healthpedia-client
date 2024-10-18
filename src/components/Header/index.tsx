b7e3d2a690268bad4a366f9ab52f2012a70b84c5
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-18 12:14:12 +0900

    fix : Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
+import HeaderLogo from "@/assets/img/header-logo.png";
 import NavLink from "@/common/NavLink";
 
 export default function Header() {
   return (
     <header className="bg-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-2xl font-bold">아프지마</div>
+        <Image src={HeaderLogo} alt="Header Logo" />
         <nav className="space-x-4">
           <NavLink href="#">제휴기업 서비스</NavLink>
           <NavLink href="#">회사소개</NavLink>
           <NavLink href="#">아프지마 소식</NavLink>
+          <NavLink href="#">제휴문의</NavLink>
+          <NavLink href="#">인재채용</NavLink>
         </nav>
       </div>
     </header>
