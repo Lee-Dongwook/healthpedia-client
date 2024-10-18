af35603f90951aa5ea70e88a998966361d4eea20
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-18 13:59:34 +0900

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,30 +3,31 @@
 import React from "react";
 import HeaderLogo from "@/assets/img/header-logo.png";
 import HeaderImage from "@/common/HeaderImage";
-import HeaderLayout from "@/common/HeaderLayout";
 import HeaderLink from "@/common/HeaderLink";
 
 export default function Header() {
   return (
-    <HeaderLayout>
-      <HeaderImage src={HeaderLogo} alt="Header Logo" />
-      <nav className="space-x-8">
-        <HeaderLink href="#" className="hover:text-blue-600">
-          제휴기업 서비스
-        </HeaderLink>
-        <HeaderLink href="#" className="hover:text-blue-600">
-          회사소개
-        </HeaderLink>
-        <HeaderLink href="#" className="hover:text-blue-600">
-          아프지마 소식
-        </HeaderLink>
-        <HeaderLink href="#" className="hover:text-blue-600">
-          제휴문의
-        </HeaderLink>
-        <HeaderLink href="#" className="hover:text-blue-600">
-          인재채용
-        </HeaderLink>
-      </nav>
-    </HeaderLayout>
+    <header className="bg-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <HeaderImage src={HeaderLogo} alt="Header Logo" />
+        <nav className="space-x-8">
+          <HeaderLink href="#" className="hover:text-blue-600">
+            제휴기업 서비스
+          </HeaderLink>
+          <HeaderLink href="#" className="hover:text-blue-600">
+            회사소개
+          </HeaderLink>
+          <HeaderLink href="#" className="hover:text-blue-600">
+            아프지마 소식
+          </HeaderLink>
+          <HeaderLink href="#" className="hover:text-blue-600">
+            제휴문의
+          </HeaderLink>
+          <HeaderLink href="#" className="hover:text-blue-600">
+            인재채용
+          </HeaderLink>
+        </nav>
+      </div>
+    </header>
   );
 }
