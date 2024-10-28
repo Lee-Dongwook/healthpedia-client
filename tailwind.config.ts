59b37505e6753d146675ce3f09880035923124aa
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-28 10:29:20 +0900

    style : Color 업데이트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,44 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        primary: "#3377FF", // Main Color
+        "primary-darker": "#2659BF", // Darker
+        "primary-lighter": "#99BBFF", // Lighter
+        "primary-subtle": "#E3EDFF", // Subtle
+
+        // State Colors
+        error: "#FF3B3B",
+        "error-darker": "#E53535",
+        "error-lighter": "#FF5C5C",
+        "error-subtle": "#FF8080",
+
+        warning: "#FFCC00",
+        "warning-darker": "#E5B800",
+        "warning-lighter": "#FDDD48",
+        "warning-subtle": "#FDED72",
+
+        info: "#0063F7",
+        "info-darker": "#004FC4",
+        "info-lighter": "#5B8DEF",
+        "info-subtle": "#9DBFF9",
+
+        success: "#06C270",
+        "success-darker": "#05A660",
+        "success-lighter": "#39D98A",
+        "success-subtle": "#57EBA1",
+
+        // Dark Colors
+        "dark-1": "#3A3A3C",
+        "dark-2": "#6B7588",
+        "dark-3": "#8F90A6",
+        "dark-4": "#C7C9D9",
+
+        // Light Colors
+        "light-1": "#DDE5E9",
+        "light-2": "#EBEBF0",
+        "light-3": "#F2F2F5",
+        "light-4": "#FAFAFC",
+
         background: "var(--background)",
         foreground: "var(--foreground)",
         romanticBlue: {
