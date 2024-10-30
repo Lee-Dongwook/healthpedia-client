c6ea00f0dbcf36aab72faf2e82d0240ce467a497
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-30 11:17:27 +0900

    fix : Header Responsive Component

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,9 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import HamburgerLogo from '@/assets/icon/etc/Hamburger.svg';
+import ShoppingBagIcon from '@/assets/icon/essentials/Shopping-bag.svg';
+import HamburgerIcon from '@/assets/icon/etc/Hamburger.svg';
+import SearchIcon from '@/assets/icon/etc/Search.svg';
 import HeaderLogo from '@/assets/img/header-logo.png';
 import HeaderItem from '@/components/molecules/HeaderItem';
 import { useResponsive } from '@/hooks/useResponsive';
@@ -11,10 +13,17 @@ const Header: React.FC = () => {
 
   if (isMobile) {
     return (
-      <header className="sticky bg-white shadow-md w-[100vw] h-[100px] p-8">
-        <div className="container mx-auto flex justify-around items-center px-4">
-          <HamburgerLogo />
+      <header className="sticky bg-white shadow-md w-[100vw] h-10 p-2">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center">
+            <HamburgerIcon />
+          </div>
+
           <Image src={HeaderLogo} alt="Header Logo" width={100} height={100} />
+          <div className="flex justify-center space-x-4">
+            <ShoppingBagIcon />
+            <SearchIcon />
+          </div>
         </div>
       </header>
     );
@@ -24,7 +33,7 @@ const Header: React.FC = () => {
     <header className="sticky bg-white shadow-md w-[100vw] h-[100px] p-8">
       <div className="container mx-auto flex justify-around items-center px-4">
         <Image src={HeaderLogo} alt="Header Logo" width={180} height={100} />
-        <nav className="space-x-10">
+        <nav className="space-x-8">
           <HeaderItem href="#services" title="제휴기업 서비스"></HeaderItem>
           <HeaderItem href="#services" title="회사소개"></HeaderItem>
           <HeaderItem href="#services" title="아프지마 소식"></HeaderItem>
