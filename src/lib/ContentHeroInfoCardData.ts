5e87bd8ad72a2c9e6a82dadfb642228ea6d1906f
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-01 12:17:19 +0900

    refactor : Content Page 리팩토링

---
+++ src/lib/ContentHeroInfoCardData.ts
@@ -0,0 +1,34 @@
+import ImagePlaceholderImage from '@/assets/img/ImagePlaceholder.png';
+
+export const ContentHeroInfoCardData = [
+  {
+    title: '수면',
+    description: '수면의 질을 향상시키고 수면 장애를 관리하는 팁을 제공합니다.',
+    imageUrl: ImagePlaceholderImage,
+    linkHref: '',
+  },
+  {
+    title: '식이',
+    description: '균형 잡힌 영양과 식습관에 대한 가이드를 제공합니다.',
+    imageUrl: ImagePlaceholderImage,
+    linkHref: '',
+  },
+  {
+    title: '비만',
+    description: '체중 관리와 건강한 생활 습관 정보를 제공합니다.',
+    imageUrl: ImagePlaceholderImage,
+    linkHref: '',
+  },
+  {
+    title: '스트레스',
+    description: '스트레스 완화 및 정신 건강을 위한 자료를 제공합니다.',
+    imageUrl: ImagePlaceholderImage,
+    linkHref: '',
+  },
+  {
+    title: '수면',
+    description: '신체 건강을 유지하기 위한 운동 루틴과 조언을 제공합니다.',
+    imageUrl: ImagePlaceholderImage,
+    linkHref: '',
+  },
+];
